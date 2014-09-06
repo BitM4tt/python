d5319279619ff25e9c40e149b47ce925950d1af5
Daniel <dan@dans.house>
2014-09-06 17:29:16 +0100

    changes to Pizza Panic made at Robert's house

--- Pizza Panic/main.py
+++ Pizza Panic/main.py
@@ -1,5 +1,5 @@
 #Pizza Panic
-import games, __init__, color
+import games, __init__, color, random
 score=0
 games.init(screen_width=800, screen_height=600, fps=100)
 chefimage=games.load_image("Chef.gif")
@@ -9,11 +9,24 @@ wallimage=games.load_image("wall.gif")
 games.screen.background=wallimage
 class Chef(games.Sprite):
     def update(self):
-        if self.right>800 or self.left<0:
+        if self.right>800:
             self.dx = -self.dx
+            bounced_pos="right"
+        if self.left<0:
+            self.dx = -self.dx
+            bounced_pos="left"
+        if bounced_pos=='left':
+            self.x+=3
+        else:
+            self.x-=3
+        n=random.randint(1,3)
+        if n==2:
+            games.screen.add(Pizza, x=self.x+120, y=self.y)
 class Pizza(games.Sprite):
     def update(self):
         image=games.load_image("Pizza.gif")
         if self.bottom>games.screen.height:
             gameovermessage=games.Message(value="GAME OVER", size=50, color=color.red, x=games.screen.width/2, y=games.screen.height/2, lifetime=5*games.screen.fps, after_death=games.screen.quit)
+            games.screen.quit()
+        self.y+=3
 games.screen.mainloop()
