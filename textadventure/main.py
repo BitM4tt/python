8f7b2a2d0f9c6f8bba8365796a208cb97e7b0f35
mcredstone <lewisdaniel86@yahoo.co.uk>
2015-06-26 11:01:35 +0100

    Create main.py

---
+++ textadventure/main.py
@@ -0,0 +1,9 @@
+import time
+
+#######################################
+# TextAdventure coded by Daniel Lewis #
+#######################################
+
+#Define functions
+def get_input(prompt, accepted):
+	
